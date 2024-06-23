8d0c761f29c02fe303331fb855dde5d716acf486
Mwti <97027005+Mwteusz@users.noreply.github.com>
2024-06-23 18:21:05 +0200

    add endpoints for graphs

--- server/eco-house2/src/main/java/com/example/ecohouse2/House.java
+++ server/eco-house2/src/main/java/com/example/ecohouse2/House.java
@@ -12,6 +12,8 @@ import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.util.*;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
+
 @Getter
 @Setter
 @Entity
@@ -42,20 +44,22 @@ public class House implements Serializable {
     private OffsetDateTime createdAt;
 
     @Column(name = "daytime_tariff", nullable = false)
-    private Double daytimeTariff;
+    private Double daytimeTariff; //PLN per kWh
 
     @Column(name = "night_tariff", nullable = false)
-    private Double nightTariff;
+    private Double nightTariff; //PLN per kWh
 
-    private LocalTime dayStart = LocalTime.of(6, 0);
-    private LocalTime dayEnd = LocalTime.of(22, 0);
+    private static LocalTime dayStart = LocalTime.parse("06:00");
+    private static LocalTime dayEnd = LocalTime.parse("22:00");
 
     public boolean isDay(LocalTime time) {
         return !time.isBefore(dayStart) && !time.isAfter(dayEnd);
     }
+    private double getTariff(LocalTime time) {
+        return isDay(time) ? daytimeTariff : nightTariff;
+    }
 
     @ManyToOne(fetch = FetchType.LAZY)
-    //@MapsId("owner_id")
     @JoinColumn(name = "owner_id", nullable = false)
     @JsonBackReference
     private Owner owner;
@@ -70,17 +74,76 @@ public class House implements Serializable {
         return convertToPowerMap(getIntervals());
     }
 
+    @JsonGetter("dailyCostGraph")
+    public Map<String, Double> calculateDailyCost() {
+        return calculateCostOfEnergy(calculateDailyEnergyConsumed());
+    }
+
+    @JsonGetter("dailyCost")
+    public double calculateDailyCostSum() {
+        Map<String, Double> costGraph = calculateDailyCost();
+        double sum = 0.0;
+        LocalTime timeBefore = null;
+        double costBefore = 0.0;
+        for (Map.Entry<String, Double> entry : costGraph.entrySet()) {
+            if (timeBefore == null) {
+                timeBefore = LocalTime.parse(entry.getKey());
+                costBefore = entry.getValue();
+                continue;
+            }
+            LocalTime current = LocalTime.parse(entry.getKey());
+
+            double hours = MINUTES.toMinutes(current.toSecondOfDay() - timeBefore.toSecondOfDay()) / (60.0 * 60.0);
+            double add = costBefore * hours;
+            //System.out.println("Time before: " + timeBefore + " Current: " + current + " Hours: " + hours + " Add: " + add);
+            sum += add;
+            timeBefore = current;
+            costBefore = entry.getValue();
+        }
+        return sum;
+    }
+
+    private Map<String, Double> calculateCostOfEnergy(Map<String, Double> dailyEnergyConsumed) {
+        Map<String, Double> result = new LinkedHashMap<>();
+        for (Map.Entry<String, Double> entry : dailyEnergyConsumed.entrySet()) {
+            LocalTime time = LocalTime.parse(entry.getKey());
+            double powerInWh = entry.getValue();
+            double powerInKwh = powerInWh / 1000.0;
+            double cost = powerInKwh * getTariff(time);
+            result.put(entry.getKey(), cost);
+        }
+        return result;
+    }
+
+
     private ArrayList<Pair<LocalTime, Double>> getIntervals() {
         double energyProduced = calculateDailyEnergyProduced();
         ArrayList<Pair<LocalTime, Double>> intervals = new ArrayList<>();
         for (Room room : rooms){
             for (Device device : room.getDevices()) {
                 for (Interval interval : device.getIntervals()) {
-                    intervals.add(Pair.of(interval.getTimeStart(), device.getPowerConsumptionPerHour() - energyProduced));
-                    intervals.add(Pair.of(interval.getTimeEnd(), -device.getPowerConsumptionPerHour() - energyProduced));
+
+                    if (isDay(interval.getTimeStart()) && isDay(interval.getTimeEnd())) { // same tariff
+                        //TODO fix if interval is longer than day
+                        intervals.add(Pair.of(interval.getTimeStart(), device.getPowerConsumptionPerHour() - energyProduced));
+                        intervals.add(Pair.of(interval.getTimeEnd(), -device.getPowerConsumptionPerHour() - energyProduced));
+                    }
+                    else if (isDay(interval.getTimeStart()) && !isDay(interval.getTimeEnd())) { //start is day, end is night
+                        intervals.add(Pair.of(interval.getTimeStart(), device.getPowerConsumptionPerHour() - energyProduced));
+                        intervals.add(Pair.of(dayEnd, -device.getPowerConsumptionPerHour() - energyProduced));
+                        intervals.add(Pair.of(dayEnd, device.getPowerConsumptionPerHour() - energyProduced));
+                        intervals.add(Pair.of(interval.getTimeEnd(), -device.getPowerConsumptionPerHour() - energyProduced));
+                    }
+                    else if (!isDay(interval.getTimeStart()) && isDay(interval.getTimeEnd())) { //start is night, end is day
+                        intervals.add(Pair.of(interval.getTimeStart(), device.getPowerConsumptionPerHour() - energyProduced));
+                        intervals.add(Pair.of(dayStart, -device.getPowerConsumptionPerHour() - energyProduced));
+                        intervals.add(Pair.of(dayStart, device.getPowerConsumptionPerHour() - energyProduced));
+                        intervals.add(Pair.of(interval.getTimeEnd(), -device.getPowerConsumptionPerHour() - energyProduced));
+                    }
                 }
             }
         }
+        System.out.println(intervals);
         return intervals;
     }
 
@@ -99,7 +162,7 @@ public class House implements Serializable {
             cumulativePower += entry.getValue();
             result.put(formatTime(entry.getKey()), cumulativePower);
         }
-
+        System.out.println(result);
         return result;
     }
 
